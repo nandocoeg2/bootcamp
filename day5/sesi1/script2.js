fbcfe6665afb51a08f811aaf7c19009f39828b80
Fernando Julian <nandokun69@gmail.com>
2023-05-29 21:01:59 +0700

    bang udah bang

--- day5/sesi1/script2.js
+++ day5/sesi1/script2.js
@@ -24,20 +24,37 @@ const detailTransaction = [
   { id: 2, productName: "Teh", qty: 3, totalAmount: 2000 },
 ];
 
-function login(username) {
+function auth(username) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const user = users.filter((user) => {
-        user.username === username;
-      });
-      if (user) {
-        resolve(user);
-      } else {
-        reject("user not found");
-      }
+      resolve(users.filter((item) => item.username === username));
     }, 500);
   });
 }
 
-console.log(login("lili"));
-// login("lili");
+function getTransaction(user_id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(
+        transaction.filter((item) => item.user_id === user_id)[0].transaction
+      );
+    }, 500);
+  });
+}
+
+function getDetailsData(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(detailTransaction.filter((item) => item.id === id)[0]);
+    }, 500);
+  });
+}
+
+async function getData(username) {
+  const user = await auth(username);
+  const transaction = await getTransaction(user[0].id);
+  const detailTransaction = await getDetailsData(transaction[0].id);
+  console.log(detailTransaction);
+}
+
+getData("lili");
